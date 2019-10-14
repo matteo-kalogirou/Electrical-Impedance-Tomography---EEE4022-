2a9e0e392e5e7ab1f359f42bd0d736d0bf7d52e4
Matteo Kalogirou <matteo.kalogirou@gmail.com>
2019-10-14 23:10:43 +0200

    Delete comma2point_overwrite.m

--- comma2point_overwrite.m
+++
@@ -1,7 +0,0 @@
-function    comma2point_overwrite( filespec )
-    file    = memmapfile( filespec, 'writable', true );
-    comma   = uint8(',');
-    point   = uint8('.');
-    file.Data( transpose( file.Data==comma) ) = point;
-    %%%delete(file)
-end
